3cb7cfd3878b244b52bb21153dd07106d5cbe7a1
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-05-05 15:42:45 +0800

    build project architectire

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 const port = 3001
+
+const routes = require('./routes');
+require('./config/mongoose')
 
 const app = express();
 
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
+
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`App is running on http://localhost:${port}`);
 })
