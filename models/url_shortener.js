7a0258547c52b2db93b3196b827910d8da1ce943
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-05-09 15:42:35 +0800

    when enter different url, produce different urlid

--- models/url_shortener.js
+++ models/url_shortener.js
@@ -1,9 +1,37 @@
 const mogoose = require('mongoose')
 const Schema = mogoose.Schema
 const urlShortenerSchema = new Schema({
+    // 指使用者輸入的網址(name="url")
     url: {
         type: String,
-    }
+        required: true
+    },
+    // 指使用者輸入的網址分配到的urlid
+    urlid: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 5
+    } 
+// }, {
+//     // 添加時間戳
+//     timestamps: true,
+//     _id: true,      // 確保 MongoDB 生成 _id
+//     id: false       // 禁用默認的 id 虛擬屬性
 })
 
+// // 創建模型前先刪除所有索引
+// const URLshortener = mogoose.model('URLshortener', urlShortenerSchema)
+// // 確保索引正確創建
+// URLshortener.on('index', error => {
+//     if (error) {
+//         console.error('索引錯誤：', error)
+//     }
+// })
+
 module.exports = mogoose.model('URLshortener', urlShortenerSchema)
+
+// 使用者表單填入文字 > 該文字有自己的id > 指定為urlid > 路由導至/urlshortener/{{urlid}} > newurl頁面產生新網址(固定+urlid)
+// 後續使用者輸入先前文字 > 尋找該文字的id > 即urlid > 路由導至/urlshortener/{{urlid}} > newurl頁面呼叫urlid對應的網址
+// /urlshortener/{{urlid}} 對應唯一的網址
--- routes/modules/urlshortener.js
+++ routes/modules/urlshortener.js
@@ -2,40 +2,72 @@ const express = require('express')
 const router = express.Router()
 const URLshortener = require('../../models/url_shortener')
 
-router.post('/newurl', (req, res) => {
-    if (!req.body.url) {
+// 建立隨機生成urlid的功能
+function generateUrlId() {
+    const num = '0123456789'
+    const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const charbox = num + alpha + alpha.toLowerCase()
+    let result = ''
+    for (let i = 0; i < 5; i++) {
+        result += charbox[Math.floor(Math.random() * charbox.length)]
+    }
+    return result
+}
+
+// 定義生成urlid的函式
+const createNewUrlID = async (url) => {
+    try {
+        let newUrlID = generateUrlId()
+        let existingUrlId = await URLshortener.findOne({ urlid: newUrlID })
+        // 先判斷urlid是否已存在，若urlid已存在，重新生成urlid
+        while (existingUrlId) {
+            newUrlID = generateUrlId()
+            existingUrlId = await URLshortener.findOne({ urlid: newUrlID })
+        }
+        // 定義文檔，確保所有必要欄位都有值
+        const newDocument = {
+            url: url,
+            urlid: newUrlID
+        }
+        // 建立新聞檔，若urlid不存在(通過while迴圈)，將文檔存入資料庫
+        return await URLshortener.create(newDocument)
+    } catch (error) {
+        console.error('Error creating new URL ID:', error)
+        throw error
+    }
+}
+
+// 使用者輸入網址後送出，到達/newurl頁面
+router.post('/newurl', async (req, res) => {
+    const url = req.body.url
+
+    // 若使用者未輸入URL (url為空，跳出請填寫url的提示)
+    if (!url) {
         req.flash('error', 'Please fill in your url.')
         return res.redirect('/')
     }
 
-    return URLshortener.create(req.body)
-        // .then(shortenurl => {
-        //     const urlData = shortenurl.toObject()
-        //     // shortenurl 是一個 Mongoose 文檔對象（Document Object），包含了從資料庫中查詢出來的資料
-        //     // toObject() 是 Mongoose 提供的方法，用來將 Mongoose 文檔轉換成普通的 JavaScript 對象
-        //     // 轉換後，urlData 就是一個純 JavaScript 對象，包含了相同的資料，但沒有 Mongoose 的特殊方法和屬性
-        //     res.render('newurl', { shortenurl: urlData })
-        // })
-        
-        .then(() => {
-            let num = '0123456789'
-            let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-            let charbox = num + alpha + alpha.toLowerCase()
-            let urlID = ''
-
-            for (let i = 0; i < 5; i++) {
-                urlID += charbox[Math.floor(Math.random() * 61)]
-                i + 1
-            }
-
-            res.render('newurl', { urlID: urlID })
-        })
-        
-        .catch(error => {
-            console.log(error)
-            req.flash('error', 'Error creating short URL')
-            return res.redirect('/')
-        })
+    try {
+        // 若使用者有輸入URL，判斷URL是否已存在
+        const existingUrl = await URLshortener.findOne({ url: url })
+        // 若URL已存在，則顯示對應到的urlid
+        if (existingUrl) {
+            let completeurl = `http://superurlshortener/${existingUrl.urlid}`
+            return res.render('newurl', { completeurl })
+        }
+        // 若URL不存在，將生成新的urlid
+        const newUrl = await createNewUrlID(url)
+        let completeurl = `http://superurlshortener/${newUrl.urlid}`
+        res.render('newurl', { completeurl })
+    } catch (error) {
+        console.log(error)
+        req.flash('error', 'Error creating short URL')
+        return res.redirect('/')
+    }
 })
 
 module.exports = router
+
+// shortenurl 是一個 Mongoose 文檔對象（Document Object），包含了從資料庫中查詢出來的資料
+// toObject() 是 Mongoose 提供的方法，用來將 Mongoose 文檔轉換成普通的 JavaScript 對象
+// 轉換後，urlData 就是一個純 JavaScript 對象，包含了相同的資料，但沒有 Mongoose 的特殊方法和屬性
